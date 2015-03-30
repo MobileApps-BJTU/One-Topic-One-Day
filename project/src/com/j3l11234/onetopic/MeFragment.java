2abee438e8c809bb4cf5419548d3ddeb7894ab7d
j3l11234 <297259024@qq.com>
2015-03-30 17:06:40 +0800

    add the base fragment

---
+++ project/src/com/j3l11234/onetopic/MeFragment.java
@@ -0,0 +1,40 @@
+package com.j3l11234.onetopic;
+
+
+import com.j3l11234.onetopic.MessageFragment.MessageAdapter;
+import com.j3l11234.onetopic.entity.MessageItem.MessageItemTag;
+
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MeFragment extends Fragment {
+
+    public MeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+    	View view = inflater.inflate(R.layout.fragment_me,container,false);
+    	
+    	
+    	
+        return view;
+    }
+
+}
